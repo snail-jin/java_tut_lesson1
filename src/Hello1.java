ac09467e0760c8a1211f0dec101b2cb7a39ffa69
snail012001@yahoo.com <snail012001@yahoo.com>
2019-06-21 18:04:58 -0700

    First commit

---
+++ src/Hello1.java
@@ -0,0 +1,11 @@
+
+public class Hello1 {
+
+	public static void main(String[] args) {
+		System.out.println("My second Java Program");
+		System.out.println("I love Java");
+		System.out.println("Hello");
+
+	}
+
+}
